618c258971e87d96a91f70138589720202e92fc7
Vibhishan Ranga <vibhishan.ranga1410@gmail.com>
2024-09-26 18:56:15 +0100

    project setup

---
+++ src/main/java/me/vibhishanranga/bookmyshowdesign/BookmyshowDesignApplication.java
@@ -0,0 +1,13 @@
+package me.vibhishanranga.bookmyshowdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmyshowDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmyshowDesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/vibhishanranga/bookmyshowdesign/BookmyshowDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package me.vibhishanranga.bookmyshowdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmyshowDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
