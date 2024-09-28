76932f3d5cc0827079ee87f0e5c0e23ece9bf997
Vibhishan Ranga <vibhishan.ranga1410@gmail.com>
2024-09-28 10:58:09 +0100

    updated application properties and user
    Signed-off-by: Vibhishan Ranga <vibhishan.ranga1410@gmail.com>

--- src/main/java/me/vibhishanranga/bookmyshowdesign/models/User.java
+++ src/main/java/me/vibhishanranga/bookmyshowdesign/models/User.java
@@ -1,8 +1,11 @@
 package me.vibhishanranga.bookmyshowdesign.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+// we change table name because postgres does not allow the creation of a table named user
 @Entity
+@Table(name = "users")
 public class User extends BaseModel{
     private String email;
 
