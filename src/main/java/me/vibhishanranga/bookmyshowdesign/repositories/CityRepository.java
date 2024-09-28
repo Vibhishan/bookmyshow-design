6f26d5f5d95a4ed23844be40b9464714b9165fa5
Vibhishan Ranga <vibhishan.ranga1410@gmail.com>
2024-09-28 13:54:15 +0100

    added code to find city by id in db

--- src/main/java/me/vibhishanranga/bookmyshowdesign/repositories/CityRepository.java
+++ src/main/java/me/vibhishanranga/bookmyshowdesign/repositories/CityRepository.java
@@ -3,6 +3,10 @@ package me.vibhishanranga.bookmyshowdesign.repositories;
 import me.vibhishanranga.bookmyshowdesign.models.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CityRepository extends JpaRepository<City, Long> {
     City save(City name);
+
+    Optional<City> findById(Long id);
 }
