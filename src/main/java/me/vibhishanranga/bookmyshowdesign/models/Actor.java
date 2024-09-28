874a6f2db9da2e4a7591e96964c402bc2d576af3
Vibhishan Ranga <vibhishan.ranga1410@gmail.com>
2024-09-28 11:10:38 +0100

    added getter and setter using lombok to actors

--- src/main/java/me/vibhishanranga/bookmyshowdesign/models/Actor.java
+++ src/main/java/me/vibhishanranga/bookmyshowdesign/models/Actor.java
@@ -2,10 +2,14 @@ package me.vibhishanranga.bookmyshowdesign.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class Actor extends BaseModel{
     private String name;
 
