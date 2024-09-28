e9c2470e666a9072afa3a00d115db97b4bb98fb2
Vibhishan Ranga <vibhishan.ranga1410@gmail.com>
2024-09-28 11:58:10 +0100

    updated create user method

--- src/main/java/me/vibhishanranga/bookmyshowdesign/controllers/UserController.java
+++ src/main/java/me/vibhishanranga/bookmyshowdesign/controllers/UserController.java
@@ -2,9 +2,26 @@ package me.vibhishanranga.bookmyshowdesign.controllers;
 
 import me.vibhishanranga.bookmyshowdesign.dtos.CreateUserRequestDto;
 import me.vibhishanranga.bookmyshowdesign.dtos.CreateUserResponseDto;
+import me.vibhishanranga.bookmyshowdesign.models.User;
+import me.vibhishanranga.bookmyshowdesign.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+// if a class is annotated this way, spring will create a controller instance without us manually creating one
+@Controller
 public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
     public CreateUserResponseDto createuser(CreateUserRequestDto request){
-        return null;
+        User savedUser = userService.createUser(request.getEmail());
+
+        CreateUserResponseDto response = new CreateUserResponseDto();
+        response.setUser(savedUser);
+        return response;
     }
 }
