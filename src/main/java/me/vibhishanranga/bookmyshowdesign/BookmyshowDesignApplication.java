5048aea7d5228340c04dbb94c0e695caded5b856
Vibhishan Ranga <vibhishan.ranga1410@gmail.com>
2024-09-28 11:58:22 +0100

    creating a user

--- src/main/java/me/vibhishanranga/bookmyshowdesign/BookmyshowDesignApplication.java
+++ src/main/java/me/vibhishanranga/bookmyshowdesign/BookmyshowDesignApplication.java
@@ -1,13 +1,28 @@
 package me.vibhishanranga.bookmyshowdesign;
 
+import me.vibhishanranga.bookmyshowdesign.controllers.UserController;
+import me.vibhishanranga.bookmyshowdesign.dtos.CreateUserRequestDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookmyshowDesignApplication {
+public class BookmyshowDesignApplication implements CommandLineRunner {
+    @Autowired
+    private UserController userController;
 
     public static void main(String[] args) {
         SpringApplication.run(BookmyshowDesignApplication.class, args);
+
+
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        CreateUserRequestDto requestDto = new CreateUserRequestDto();
+        requestDto.setEmail("test_user2@gmail.com");
+
+        this.userController.createuser(requestDto);
+    }
 }
