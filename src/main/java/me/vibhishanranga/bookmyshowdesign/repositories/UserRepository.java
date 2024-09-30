445a8df64e53ceb3cb14afda3dc07c560db5b2ea
Vibhishan Ranga <vibhishan.ranga1410@gmail.com>
2024-09-30 18:34:26 +0100

    added method to find user by id

--- src/main/java/me/vibhishanranga/bookmyshowdesign/repositories/UserRepository.java
+++ src/main/java/me/vibhishanranga/bookmyshowdesign/repositories/UserRepository.java
@@ -4,7 +4,11 @@ import me.vibhishanranga.bookmyshowdesign.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User save(User user);
+
+    Optional<User> findById(Long id);
 }
